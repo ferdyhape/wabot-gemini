519cbd9285ed72346656639421b7750d02747013
Ferdy Hahan Pradana <ferdyhahan5@gmail.com>
2024-06-19 02:02:28 +0700

    integrating with gemini ai

--- index.js
+++ index.js
@@ -1,74 +1,73 @@
-import {
-  GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
-} from "@google/generative-ai";
-import chalk from "chalk";
-import ora from "ora";
-import prompt from "prompt-sync";
-import "dotenv/config";
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+require("dotenv").config();
 
-const promptSync = prompt();
-const MODEL_NAME = "gemini-1.0-pro";
-const API_KEY = process.env.GEMINI_API_KEY;
-const GENERATION_CONFIG = {
-  temperature: 0.9,
-  topK: 1,
-  topP: 1,
-  maxOutputTokens: 2048,
-};
-const SAFETY_SETTINGS = [
-  {
-    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+//Creating instances
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox", "--disable-gpu"],
   },
-  {
-    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
   },
-  {
-    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  },
-  {
-    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-  },
-];
+});
 
-async function runChat() {
-  const spinner = ora("Initializing chat...").start();
-  try {
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+//Initializing GenAI model
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-    const chat = model.startChat({
-      generationConfig: GENERATION_CONFIG,
-      safetySettings: SAFETY_SETTINGS,
-      history: [],
-    });
+//Function to generate reponse from AI model and reply to user
+async function generate(prompt, message) {
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
 
-    spinner.stop();
-
-    while (true) {
-      const userInput = promptSync(chalk.green("You: "));
-      if (userInput.toLowerCase() === "exit") {
-        console.log(chalk.yellow("Goodbye!"));
-        process.exit(0);
-      }
-      const result = await chat.sendMessage(userInput);
-      if (result.error) {
-        console.error(chalk.red("AI Error:"), result.error.message);
-        continue;
-      }
-      const response = result.response.text();
-      console.log(chalk.blue("AI:"), response);
-    }
-  } catch (error) {
-    spinner.stop();
-    console.error(chalk.red("An error occurred:"), error.message);
-    process.exit(1);
-  }
+  await message.reply(text); //Reply to user
 }
 
-runChat();
+//All event listener to know client status
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("authenticated", () => {
+  console.log("Client is authenticated!");
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("disconnected", () => {
+  console.log("Client is disconnected!");
+});
+
+client.on("auth_failure", () => {
+  console.log("Client is auth_failure!");
+});
+
+client.on("message", async (message) => {
+  if (message.body.includes(".bot")) {
+    var query;
+    //Extracting text from message body using regular expression method
+    const regxmatch = message.body.match(/.bot(.+)/);
+
+    //If no text followed by .bot then we use "Hi" as text
+    if (regxmatch) {
+      query = regxmatch[1];
+    } else {
+      console.log("No regex match!");
+      query = "Hi";
+    }
+
+    //Call the generate function
+    generate(query, message);
+  }
+});
+
+client.initialize();
